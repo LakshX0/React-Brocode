78fe305183b66413605e0630a516e8ada70e388c
LakshX0 <lakshansandeep7@gmail.com>
2024-11-02 00:59:45 +0530

    added card updates and props

--- brocode/src/App.jsx
+++ brocode/src/App.jsx
@@ -1,11 +1,20 @@
+import Card from "./Card.jsx";
 import Food from "./Food.jsx";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
+import Student from "./Student.jsx";
+// import Button from "./Button.jsx";
 
 function App() {
   return (
     <>
       <Header />
+      <Card />
+      <Card />
+      <Card />
+      {/* <Button /> */}
+      <Student name="Lakshan" age={29} isStudent={true} />
+      <Student name="Perera" age={43} isStudent={false} />
       <Food />
       <Footer />
     </>
