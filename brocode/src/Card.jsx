aa54a4bd09274b2ddad0c25b327c75560592cece
LakshX0 <lakshansandeep7@gmail.com>
2024-11-01 13:22:49 +0530

    added card and few more components

---
+++ brocode/src/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+  return (
+    <div className="card">
+      <img src="" alt="" />
+      <h2>Lakshan</h2>
+      <p>
+        I am Lakshan. I am a Front-End Developer. Currently i am learnin react.
+        I love to read books. I also do trading as a part-timer😋
+      </p>
+    </div>
+  );
+}
+
+export default Card;
---
+++ brocode/src/Food.jsx
@@ -0,0 +1,13 @@
+function Food() {
+  const food1 = "Mango";
+  const food2 = "Apple";
+
+  return (
+    <ul>
+      <li>Berry</li>
+      <li>{food1}</li>
+      <li>{food2}</li>
+    </ul>
+  );
+}
+export default Food;
