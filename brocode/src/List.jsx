215a9460abd9c8c36f1dd663a48f4f2457be546b
LakshX0 <lakshansandeep7@gmail.com>
2024-11-20 00:38:04 +0530

    updated list code

--- brocode/src/List.jsx
+++ brocode/src/List.jsx
@@ -1,6 +1,17 @@
 function List() {
-  const fruit = ["Banana", "Apple", "Orage", "Grape", "Mango", "Pineapple"];
-  const fruitItems = fruit.map((fruit) => <li>{fruit}</li>);
+  const fruit = [
+    { id: 1, name: "Banana", calories: 95 },
+    { id: 2, name: "Apple", calories: 105 },
+    { id: 3, name: "Orage", calories: 68 },
+    { id: 4, name: "Grape", calories: 60 },
+    { id: 5, name: "Mango", calories: 50 },
+    { id: 6, name: "Pineapple", calories: 112 },
+  ];
+  const fruitItems = fruit.map((fruit) => (
+    <li key={fruit.id}>
+      {fruit.name}: &nbsp; <b>{fruit.calories}</b>
+    </li>
+  ));
 
   return <ol>{fruitItems}</ol>;
 }
